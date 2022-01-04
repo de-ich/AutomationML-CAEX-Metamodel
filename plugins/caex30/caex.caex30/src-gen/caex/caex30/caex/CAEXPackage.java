3e02765ff3569ec8ac4e84dbc633a92b32f19d97
Matthias Freund <matthias.freund@festo.com>
2022-01-04 11:28:47 +0100

    Regenerate model code

--- plugins/caex30/caex.caex30/src-gen/caex/caex30/caex/CAEXPackage.java
+++ plugins/caex30/caex.caex30/src-gen/caex/caex30/caex/CAEXPackage.java
@@ -43,7 +43,7 @@ public interface CAEXPackage extends EPackage {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	String eNS_URI = "http://www.dke.de/CAEX/300";
+	String eNS_URI = "http://www.dke.de/CAEX";
 
 	/**
 	 * The package namespace name.
